7f5e01a5889039691946c318f64e6cee3e9ee370
Virus936 <lok.vira@gmail.com>
2021-05-24 19:33:27 +0200

    intersection observer for stylier display

---
+++ app.js
@@ -0,0 +1,23 @@
+const options = {
+  threshold:0.2
+}
+
+const display = (entries, observeur) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1
+      entry.target.style.transform = 'translateX(0)'
+    }
+    else{
+      entry.target.style.opacity=0
+      entry.target.style.transform = 'translateX(-20px)'
+    }
+    console.log( entry.target.style.opacity)
+  })
+  
+}
+const observer = new IntersectionObserver(display, options)
+
+
+sections = document.querySelectorAll('section')
+sections.forEach(section=>observer.observe(section))
